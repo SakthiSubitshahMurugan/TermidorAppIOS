414c63bc4a8bd2a5ff84607a565b630949eb3b5b
Sakthi Subitsah <subitsahs@was1055-subisal.local>
2017-12-06 14:10:29 -0500

    Initial Commit

---
+++ TermidorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TermidorApp
+//
+//  Created by Subitsah, Sakthi on 12/6/17.
+//  Copyright © 2017 Subitsah, Sakthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
